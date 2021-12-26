d9ce4b90303dbc74535f9d9d0319f9c7035040d8
tangcong <tangcong506@foxmail.com>
2021-12-26 08:11:15 +0800

    ci: add global.kstone.tag for test env

--- test/scripts/kstone.sh
+++ test/scripts/kstone.sh
@@ -3,3 +3,5 @@
 set -ex
 kubectl create ns kstone
 cd charts && helm install kstone . -n kstone -f values.test.yaml --set global.kstone.tag=$VERSION
+echo "kstone component images:"
+kubectl get deployment -n kstone -o yaml | grep image: | grep -v "f:image:"
